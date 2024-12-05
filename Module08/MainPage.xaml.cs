94602ac7784f3a023713d65379b361ccfedb9824
tmsc14 <cabanayan.tereemari@auf.edu.ph>
2024-12-05 20:01:23 +0800

    Added db connection status

--- Module08/MainPage.xaml.cs
+++ Module08/MainPage.xaml.cs
@@ -30,6 +30,34 @@
 		{
 			await Shell.Current.GoToAsync("//StudentListPage");
 		}
-	}
+
+        private async void OnTestDbConnection(object sender, EventArgs e)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync("http://localhost/pdc50/test_connection.php");
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        ConnectionStatus.Text = "Database Connection Successful";
+                        ConnectionStatus.TextColor = Colors.Green;
+                    }
+                    else
+                    {
+                        ConnectionStatus.Text = "Database Connection Failed";
+                        ConnectionStatus.TextColor = Colors.Red;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ConnectionStatus.Text = $"Error: {ex.Message}";
+                ConnectionStatus.TextColor = Colors.Red;
+            }
+        }
+    }
 
 }
